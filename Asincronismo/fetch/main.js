ac123cb8edbc790e7672a82653d5b992e815bcbd
callm3thebreeze <jesusvillarjimenez@gmail.com>
2022-03-13 22:04:44 +0100

    asincronismo completado

--- Asincronismo/fetch/main.js
+++ Asincronismo/fetch/main.js
@@ -3,30 +3,69 @@
 //stringify() convirte un objeto de JS en una cadena JSON
 
 
-const contenedor = document.querySelector('#resultado');
+// const contenedor = document.querySelector('#resultado');
 
-fetch('http://localhost:3000/api')
-.then(respuestaHTTP => {
-    console.log(respuestaHTTP);
-    return respuestaHTTP.json();
-})
-.then(texto => {
-    console.log(texto.datos[0]);
-    contenedor.innerHTML = texto.datos[0].title;
-});
+// fetch('http://localhost:3000/api')
+// .then(respuestaHTTP => {
+//     console.log(respuestaHTTP);
+//     return respuestaHTTP.json();
+// })
+// .then(texto => {
+//     console.log(texto.datos[0]);
+//     contenedor.innerHTML = texto.datos[0].title;
+// });
 
 
-fetch('http://localhost:3000', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({'curso': 'Javascript'})
-})
-.then(respuestaHTTP => respuestaHTTP.json())
-.then(json =>{
-    console.log(json);
-})
-.catch(error => {
-    console.error(error);
-});
+// fetch('http://localhost:3000', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({'curso': 'Javascript'})
+// })
+// .then(respuestaHTTP => respuestaHTTP.json())
+// .then(json =>{
+//     console.log(json);
+// })
+// .catch(error => {
+//     console.error(error);
+// });
+
+
+//FETCH CON ASYN Y AWAIT
+
+//1. DENTRO DE UNA FUNCION 
+
+// async function get(){
+//     const resultadoHttp = await fetch('http://localhost:3000/api')
+//     const data = await resultadoHttp.json();
+
+//     console.log('Mensaje =', data.mensaje);
+// }
+
+// get();
+
+
+//2. CON UNA FNCION AUTOEJECUTABLE (FUNCION MAGICA)
+
+( async () => {
+    const resultadoHttp = await fetch('http://localhost:3000/api')
+    const data = await resultadoHttp.json();
+
+    console.log('Mensaje =', data.mensaje);
+
+})();
+
+//3. CON FUNCION MAGICA Y TRY CATCH
+
+( async () => {
+    try{
+        const resultadoHttp = await fetch('http://localhost:3000/api')
+        const data = await resultadoHttp.json();
+    
+        console.log('Mensaje =', data.mensaje);
+    }catch(error){
+        console.error(error);
+    }  
+
+})();
